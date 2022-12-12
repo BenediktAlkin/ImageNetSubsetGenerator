06e4f228ee0cddae143de758da1dbc6699b3597a
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-12-12 23:43:15 +0100

    change to links

--- imagenet_subset_generator/generate_subset.py
+++ imagenet_subset_generator/generate_subset.py
@@ -5,27 +5,22 @@ from pathlib import Path
 
 from .util import parse_version
 
-def generate_subset(in1k_path, out_path, version, train, val, log=print):
+def generate_subset(in1k_path, out_path, version, log=print):
     in1k_path = Path(in1k_path).expanduser()
     out_path = Path(out_path).expanduser()
     assert in1k_path.exists(), f"invalid path to ImageNet1K: {in1k_path}"
     nonempty_outpath_msg = f"out_path already exists and is not empty: {out_path}"
     assert not out_path.exists() or len(os.listdir(out_path)) == 0, nonempty_outpath_msg
-    splits = []
-    if train:
-        splits.append("train")
-    if val:
-        splits.append("val")
 
     # get classes and info
     classes, files, info = parse_version(version=version, log=log)
     # max number of digits
     lpad = len(str(len(classes))) if classes is not None else 0
 
-    # copy folders
+    # link folders
     out_path.mkdir(exist_ok=True)
     if classes is not None:
-        for split in splits:
+        for split in ["train", "val"]:
             i = 0
             log(f"processing {split}")
             split_path = in1k_path / split
@@ -37,14 +32,11 @@ def generate_subset(in1k_path, out_path, version, train, val, log=print):
                 if folder in classes:
                     i += 1
                     istr = str(i).zfill(lpad)
-                    log(f"processing {split}/{folder} ({istr}/{len(classes)})")
-                    if mode in ["all", "imagefolder"]:
-                        shutil.copytree(split_path / folder, split_out_path / folder)
-                    filelist[folder] = list(sorted(os.listdir(split_path / folder)))
-            log(f"finished copying {split}")
+                    log(f"linking {split}/{folder} ({istr}/{len(classes)})")
+                    os.symlink(split_path / folder, split_out_path / folder, target_is_directory=True)
 
-    # copy files (only train)
-    if train and files is not None:
+    # link files (only makes sense for train split)
+    if files is not None:
         split_path = in1k_path / "train"
         split_out_path = out_path / "train"
         split_out_path.mkdir(exist_ok=True, parents=True)
@@ -53,17 +45,16 @@ def generate_subset(in1k_path, out_path, version, train, val, log=print):
             folder = file.split("_")[0]
             src_file = split_path / folder / file
             assert src_file.exists()
-            log(f"processing {file}")
+            log(f"linking {file}")
             dst_folder = split_out_path / folder
             dst_folder.mkdir(exist_ok=True)
-            shutil.copy(src_file,  dst_folder / file)
+            os.symlink(src_file, dst_folder / file, target_is_directory=False)
 
-    # copy metafile
+    # link metafile
     meta_path = in1k_path / "meta.bin"
     if meta_path.exists():
-        log("copying meta.bin")
-        shutil.copy(in1k_path / "meta.bin", out_path / "meta.bin")
-        log("copied meta.bin")
+        log("linking meta.bin")
+        os.symlink(in1k_path / "meta.bin", out_path / "meta.bin", target_is_directory=False)
     else:
         log(f"no meta.bin file found")
 
--- main_subset.py
+++ main_subset.py
@@ -8,8 +8,6 @@ def parse_args():
     parser.add_argument("--in1k_path", required=True, help="path to the directory where imagenet is stored")
     parser.add_argument("--out_path", required=True, help="path to the directory where imagenet is stored")
     parser.add_argument("--version", choices=VERSIONS, help="use predefined classes from a popular version")
-    parser.add_argument("--train", action="store_true", help="generate the train dataset")
-    parser.add_argument("--val", action="store_true", help="generate the val dataset")
     return parser.parse_args()
 
 
