2475bca47a307595fb4c3c2f837eacb574eea2d2
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-17 15:26:21 +0100

    subsets

---
+++ imagenet_subset_generator/versions/in100_sololearn_msn5perclass_split3.py
@@ -0,0 +1,8 @@
+from .in1k_msn5perclass_split3 import FILES as _FILES
+from .in100_sololearn import CLASSES as _CLASSES
+
+INFO = [
+    "in1k_msn5perclass_split3 with only the classes of in100_sololearn"
+]
+
+FILES = [f for f in _FILES if f.split("_")[0] in _CLASSES]
