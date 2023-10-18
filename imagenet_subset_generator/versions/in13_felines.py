7c23d2e2cb81091b142d5bdab4fee1138c2b27ee
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-10-19 01:09:34 +0200

    dogs felines

---
+++ imagenet_subset_generator/versions/in13_felines.py
@@ -0,0 +1,19 @@
+INFO = [
+    "https://observablehq.com/@mbostock/imagenet-hierarchy",
+    "all 13 feline classes of ImageNet",
+]
+CLASSES = [
+    "n02123045",
+    "n02123159",
+    "n02123394",
+    "n02123597",
+    "n02124075",
+    "n02125311",
+    "n02127052",
+    "n02128385",
+    "n02128757",
+    "n02128925",
+    "n02129165",
+    "n02129604",
+    "n02130308",
+]
